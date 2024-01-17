7604444657812448237cd4311a239534175745c5
jervincarlos <jervin@sliqpay.com>
2024-01-17 23:57:35 +0800

    Add api urls

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class LoginService {
   constructor(private httpClient: HttpClient) {}
 
   login(payload: { username: string; password: string }) {
-    return this.httpClient.post('', payload);
+    return this.httpClient.post(`${environment.apiUrl}/login`, payload);
   }
 }
